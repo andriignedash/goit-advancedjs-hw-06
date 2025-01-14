f0dca1f94a22c7de4e75a55e775e8694bccd2391
Andrii Gnedash <andrii.gnedash@ringcentral.com>
2025-01-14 12:09:22 +0100

    hw

--- src/generic/7.ts
+++ src/generic/7.ts
@@ -4,16 +4,16 @@
 */
 
 export enum UserRole {
-  admin = 'admin',
-  editor = 'editor',
-  guest = 'guest',
+  admin = "admin",
+  editor = "editor",
+  guest = "guest",
 }
 
-// Замініть наступний код на версію за допомогою Record
-const RoleDescription = {
-  admin: 'Admin User',
-  editor: 'Editor User',
-  guest: 'Guest User',
+// Використання Record для визначення об'єкта RoleDescription
+const RoleDescription: Record<UserRole, string> = {
+  [UserRole.admin]: "Admin User",
+  [UserRole.editor]: "Editor User",
+  [UserRole.guest]: "Guest User",
 };
 
 export {};
